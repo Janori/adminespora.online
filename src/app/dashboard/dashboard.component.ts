9d0d580812ff551bdcfc7165aa373a71cd130475
Edgar Sandoval <edgarsandovalavalos@hotmail.com>
2017-10-07 02:53:00 -0500

    Pantilla base para dashboard.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,29 +13,14 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   @HostBinding('@routerAnimation') routerAnimation = true;
   // Applying theme class
   @HostBinding('class.dark-theme') darkTheme = false;
-  _sidenavMode = 'side';
+  _sidenavMode = 'over';
   _boxedLayout = false;
   // Data for messages at popup
-  messages = [
-    {
-      subject: 'Monthly report',
-      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-      image: 'assets/avatars/4040.png',
-      time: '18:05'
-    },
-    {
-      subject: 'Holiday party',
-      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-      image: 'assets/avatars/4040.png',
-      time: '3 hrs ago'
-    },
-    {
-      subject: 'Salary bonus',
-      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-      image: 'assets/avatars/4040.png',
-      time: '2 days ago'
-    }
-  ];
+
+  public user: object = {
+    name: 'Administrador Espora',
+    kind: 'Administrador',
+  };
 
 
   constructor(public resizeService: ResizeService) {
@@ -46,7 +31,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     // Resize after sidenav open on startup to draw charts correctly
-    setTimeout(() => this.resizeService.resizeInformer$.next(), 500);
+    // setTimeout(() => this.resizeService.resizeInformer$.next(), 500);
   }
 
   set sidenavMode(val) {
